3ca526545ec1671be0155a982bdcf72816c48d98
robim16 <carteagajimenez@gmail.com>
2025-11-06 16:00:56 -0500

    reglas de validacion en dto

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -24,7 +24,7 @@ namespace Play.Catalog.Service.Controllers
         }
 
         [HttpGet("{id}")]
-        public ItemDto GetById(Guid id)
+        public ActionResult<ItemDto> GetById(Guid id)
         {
             var item = items.Where(item => item.Id == id).SingleOrDefault();
             return item;
@@ -44,6 +44,11 @@ namespace Play.Catalog.Service.Controllers
         {
             var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
 
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
             var updatedItem = existingItem with
             {
                 Name = updateItemDto.Name,
@@ -53,6 +58,21 @@ namespace Play.Catalog.Service.Controllers
 
             var index = items.FindIndex(existingItem => existingItem.Id == id);
             items[index] = updatedItem;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+
+            if (index < 0)
+            {
+                return NotFound();
+            }
+            
+            items.RemoveAt(index);
 
             return NoContent();
         }
